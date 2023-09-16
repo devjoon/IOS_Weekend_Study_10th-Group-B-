2126fdd1cddf5c6d85bcac03c7ca51551bb3366c
Morgan <101351216+devjoon@users.noreply.github.com>
2023-09-16 09:46:27 +0900

    feat: ex1 제작

--- Week3/Experiment_MVC/ViewController.swift
+++ Week3/Experiment_MVC/ViewController.swift
@@ -6,7 +6,8 @@
 import UIKit
 
 struct Registrant {
-
+    var name: String
+    var phoneNumber: String
 }
 
 class ViewController: UIViewController {
@@ -16,19 +17,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var phoneNumberLabel: UILabel!
     
-    var registrantList: [Registrant] = []
+    private var registrantList: [Registrant] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
     @IBAction func hitRegisterButton(_ sender: Any) {
-        
+        let name = nameTextField.text ?? ""
+        let phoneNumber = phoneNumberTextField.text ?? ""
+        let registrant = Registrant(name: name, phoneNumber: phoneNumber)
+        registrantList.append(registrant)
     }
     
     @IBAction func hitCheckButton(_ sender: Any) {
-        
+        nameLabel.text = registrantList.last?.name
+        phoneNumberLabel.text = registrantList.last?.phoneNumber
     }
     
     func register() {
