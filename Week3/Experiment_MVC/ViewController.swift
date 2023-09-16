4b2bcd75a1a21dcda433bbc1cf207f6edbc75bcb
kiseok <carti1108@naver.com>
2023-09-16 11:18:41 +0900

    실험 3 구현

--- Week3/Experiment_MVC/ViewController.swift
+++ Week3/Experiment_MVC/ViewController.swift
@@ -5,6 +5,8 @@
 
 import UIKit
 
+let notificationCenter: NotificationCenter = .default
+
 struct Registrant {
     var name: String
     var phoneNumber: String
@@ -22,25 +24,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        notificationCenter.addObserver(self, selector: #selector(register), name: .regist, object: nil)
     }
+    
 
     @IBAction func hitRegisterButton(_ sender: Any) {
-        let name = nameTextField.text ?? ""
-        let phoneNumber = phoneNumberTextField.text ?? ""
-        let registrant = Registrant(name: name, phoneNumber: phoneNumber)
-        registrantList.append(registrant)
+        notificationCenter.post(name: .regist, object: nil)
     }
     
     @IBAction func hitCheckButton(_ sender: Any) {
+        
+    }
+    
+    @objc func register() {
+        let name = nameTextField.text ?? ""
+        let phoneNumber = phoneNumberTextField.text ?? ""
+        let registrant = Registrant(name: name, phoneNumber: phoneNumber)
+        
+        registrantList.append(registrant)
         nameLabel.text = registrantList.last?.name
         phoneNumberLabel.text = registrantList.last?.phoneNumber
     }
-    
-    func register() {
-        
-    }
+}
+
+extension Notification.Name {
+    static let regist = Notification.Name("regist")
 }
 
 // 뷰와 컨트롤러는 나누기 어렵다.
 // 어려운 이유는 컨트롤러가 뷰를 포함할 뿐만 아니라 라이프사이클을 관리하기 떄문에 서로 밀접하게 연관되어 있어서
 // 뷰를 포함할뿐 뷰라고 보기에는 어려움이 있다.
+
