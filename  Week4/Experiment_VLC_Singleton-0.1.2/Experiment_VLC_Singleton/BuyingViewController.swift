aac52ad76a9c15c0cafac0c4addf8e60c1c769ed
ue-mu <ym14170@gmail.com>
2023-09-23 09:41:07 +0900

    step1 구현

---  Week4/Experiment_VLC_Singleton-0.1.2/Experiment_VLC_Singleton/BuyingViewController.swift
+++  Week4/Experiment_VLC_Singleton-0.1.2/Experiment_VLC_Singleton/BuyingViewController.swift
@@ -7,8 +7,35 @@ import UIKit
 
 class BuyingViewController: UIViewController {
     
+    override func loadView() {
+        super.loadView()
+        print("2 - loadView")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("2 - viewDidLoad")
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("2 - viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("2 - viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("2 - viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("2 - viewDidDisappear")
     }
     
     @IBAction func dismissButton(_ sender: Any) {
---  Week4/Experiment_VLC_Singleton-0.1.2/Experiment_VLC_Singleton/MainViewController.swift
+++  Week4/Experiment_VLC_Singleton-0.1.2/Experiment_VLC_Singleton/MainViewController.swift
@@ -11,9 +11,37 @@ class PointManager {
 
 class MainViewController: UIViewController {
     
+    override func loadView() {
+        super.loadView()
+        print("1 - loadView")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("1 - viewDidLoad")
         
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("1 - viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("1 - viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("1 - viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("1 - viewDidDisappear")
+    }
+    
+    
 }
 
