d4cc40f3eaa26687a6a14731cbc84effe3cf9412
ue-mu <ym14170@gmail.com>
2023-11-04 09:41:11 +0900

    feat: tableView 실험1

---
+++ Week8/Table View/Table_View/Table_View/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Table_View
+//
+//  Created by uemu on 2023/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+}
+
