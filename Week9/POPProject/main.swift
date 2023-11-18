a2ca08474c5a872101d9c82d919de8a2ca00144b
Morgan <devjoon@naver.com>
2023-11-18 10:16:21 +0900

    MacBook구현

--- Week9/POPProject/main.swift
+++ Week9/POPProject/main.swift
@@ -5,5 +5,29 @@
 //  Created by hyunMac on 11/18/23.
 //
 
+import Foundation
+
 typealias WattPerHour = Int
 typealias Watt = Int
+
+struct MacBook {
+    var limitFullChargingWatt: Watt
+    var batteryVolume: Watt
+    var fullBatteryVolume: Watt = 100
+    
+    init(limitFullChargingWatt: Watt, batteryVolume: Watt) {
+        self.limitFullChargingWatt = limitFullChargingWatt
+        self.batteryVolume = batteryVolume
+    }
+    
+    mutating func chargeBattery(charger: Chargeable) {
+        let chargingTime = 1
+        var fullchargingTime = 0
+        while fullBatteryVolume > batteryVolume {
+            sleep(UInt32(chargingTime))
+            fullchargingTime += chargingTime
+            batteryVolume += charger.maximumWattPerHour
+        }
+        print("충전에 걸린시간은 \(fullchargingTime) 입니다.")
+    }
+}
